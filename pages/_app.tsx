c7bf1392b6e0cdcccac0d6ba3a9121dbbb752a0d
Petr Cibulka <cibulka.me@gmail.com>
2021-12-16 12:46:12 +0100

    injectOrder

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,20 @@
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from '@mui/material/styles';
+import { StyledEngineProvider } from '@mui/material/styles';
+
+import muiTheme from '../theme/mui';
 
 import '../styles/tailwind.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  return (
+    <StyledEngineProvider injectFirst>
+    <ThemeProvider theme={muiTheme}>
+        <Component {...pageProps} />
+    </ThemeProvider>
+    </StyledEngineProvider>
+  );
 }
 
 export default MyApp
