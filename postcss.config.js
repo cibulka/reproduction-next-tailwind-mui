cba38ca9c8ab29f320743d6324d841b692687289
Petr Cibulka <cibulka.me@gmail.com>
2021-12-16 12:04:15 +0100

    first commit

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: {
+    tailwindcss: {
+      config: 'theme/tailwind.js',
+    },
+    autoprefixer: {},
+  },
+};
---
+++ theme/tailwind.js
@@ -0,0 +1,7 @@
+const tailwind = {
+  darkMode: 'class', // or 'media',
+  theme: {},
+  content: ['./pages/**/*.tsx', './components/**/*.tsx', './containers/**/*.tsx'],
+};
+
+module.exports = tailwind;
